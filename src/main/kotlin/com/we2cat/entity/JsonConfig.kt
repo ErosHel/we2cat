8846448a9d39c3a09973bfa6340231f8fc237fb6
hel <i@hel.ink>
2021-02-02 18:28:24 +0800

    优化

--- src/main/kotlin/com/we2cat/entity/JsonConfig.kt
+++ src/main/kotlin/com/we2cat/entity/JsonConfig.kt
@@ -19,10 +19,16 @@ class JsonConfig {
      * 生成json包白名单
      */
     var whitelist: ArrayList<String> = arrayListOf()
+        set(value) {
+            field = getOrElse(value, field)
+        }
 
     /**
      * 生成json包黑名单
      */
     var blacklist: ArrayList<String> = arrayListOf("java.*", "sun.*")
+        set(value) {
+            field = getOrElse(value, field)
+        }
 
 }
